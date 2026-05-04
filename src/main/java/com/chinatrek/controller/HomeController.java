449463ff6de7de647c57df61333ec75dd788a4ae
HasanRinat <hasanrinatahmet@gmail.com>
2026-05-05 00:33:55 +0500

    Update HomeController.java

--- src/main/java/com/chinatrek/controller/HomeController.java
+++ src/main/java/com/chinatrek/controller/HomeController.java
@@ -1,10 +1,18 @@
 package com.chinatrek.controller;
 
+import com.chinatrek.model.User;
+import com.chinatrek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    
+    @Autowired
+    private UserService userService;
     
     @GetMapping("/")
     public String home() {
@@ -12,22 +20,33 @@ public class HomeController {
     }
     
     @GetMapping("/register")
-    public String register() {
+    public String registerPage() {
         return "register";
     }
     
     @GetMapping("/login")
-    public String login() {
+    public String loginPage() {
         return "login";
     }
     
     @GetMapping("/map")
-    public String map() {
+    public String mapPage(@RequestParam String email, Model model) {
+        User user = userService.getUser(email);
+        if (user == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("user", user);
         return "map";
     }
     
     @GetMapping("/quiz")
-    public String quiz() {
+    public String quizPage(@RequestParam String email, @RequestParam int locationIndex, Model model) {
+        User user = userService.getUser(email);
+        if (user == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("locationIndex", locationIndex);
         return "quiz";
     }
 }
