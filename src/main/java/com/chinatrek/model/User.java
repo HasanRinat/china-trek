6f830086dcb38f1bbd09733c26f9b646bf7536dc
HasanRinat <hasanrinatahmet@gmail.com>
2026-05-05 00:32:27 +0500

    Update User.java

--- src/main/java/com/chinatrek/model/User.java
+++ src/main/java/com/chinatrek/model/User.java
@@ -1,8 +1,5 @@
 package com.chinatrek.model;
 
-import lombok.Data;
-
-@Data
 public class User {
     private String email;
     private String username;
@@ -14,7 +11,25 @@ public class User {
         this.email = email;
         this.username = username;
         this.balance = 100;  // Стартовый баланс
-        this.currentLocationIndex = 0;
+        this.currentLocationIndex = 0;  // Первая локация
         this.character = "🐼 Панда";
     }
+    
+    // Геттеры и сеттеры
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    
+    public int getBalance() { return balance; }
+    public void setBalance(int balance) { this.balance = balance; }
+    
+    public int getCurrentLocationIndex() { return currentLocationIndex; }
+    public void setCurrentLocationIndex(int currentLocationIndex) { 
+        this.currentLocationIndex = currentLocationIndex; 
+    }
+    
+    public String getCharacter() { return character; }
+    public void setCharacter(String character) { this.character = character; }
 }
