010584eb5c278173bd7d1ec6cbd2bb82b463e8a3
HasanRinat <hasanrinatahmet@gmail.com>
2026-05-04 23:14:13 +0500

    Create ChinaTrekApplication.java

---
+++ src/main/java/com/chinatrek/ChinaTrekApplication.java
@@ -0,0 +1,12 @@
+package com.chinatrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaTrekApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChinaTrekApplication.class, args);
+        System.out.println("🎉 China Trek запущен! Откройте http://localhost:8080");
+    }
+}
